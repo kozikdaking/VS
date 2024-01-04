3e9311b90f6c2b135614a9ec538adf65c7690ee9
paweu9000 <wolfenorrin5@gmail.com>
2024-01-04 11:36:40 +0100

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window: exit
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+ 
+        // Clear screen
+        window.clear();
+ 
+        // Update the window
+        window.display();
+    }
+ 
+    return EXIT_SUCCESS;
 }
